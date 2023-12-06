b78d9a1c18d5f8b57e8ee087976c4d767b765089
Hsiang <d9971322@gmail.com>
2023-12-06 14:23:43 +0800

    initial

---
+++ 19_單檔案元件/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+import School from './School';
+import Student from "./Student";
+
+export default {
+ name: "App",
+ //彙總所有的元件
+ components:{
+   Student,
+   School
+ }
+}
+</script>
+
